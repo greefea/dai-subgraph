7b0b57e83f71c9efaeaa272dcaf2b34c0df164eb
wyaming89 <wyaming89@gmail.com>
2022-08-27 15:19:38 +0800

    add transfer event function handler

--- src/dai.ts
+++ src/dai.ts
@@ -3,14 +3,24 @@ import { DAI, Approval, LogNote, Transfer } from "../generated/DAI/DAI";
 import { token } from "../generated/schema";
 
 export function handleTransfer(event: Transfer): void {
-  let dai = token.load(event.address.toHexString());
-  if (dai == null) {
-    dai = new token(event.address.toHexString());
-  }
-  let daiContract = DAI.bind(event.address);
-  dai.decimals = daiContract.decimals();
-  dai.symbol = daiContract.symbol();
-  dai.supply = daiContract.totalSupply();
+//   let dai = token.load(event.address.toHexString());
+//   if (dai == null) {
+//     dai = new token(event.address.toHexString());
+//   }
+//   let daiContract = DAI.bind(event.address);
+//   dai.decimals = daiContract.decimals();
+//   dai.symbol = daiContract.symbol();
+//   dai.supply = daiContract.totalSupply();
 
-  dai.save();
+//   dai.save();
+
+    let dai = token.load(event.address.toHexString());
+    if(dai == null) {
+        dai = new token(event.address.toHexString());
+    }
+    dai.to = event.params.dst.toHexString();
+    dai.from = event.params.src.toHexString();
+    dai.amount = event.params.wad;
+
+    dai.save();
 }
