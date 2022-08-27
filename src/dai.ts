89c11837ec7b1aeedbae1d56485750eac9eddfe5
wyaming89 <wyaming89@gmail.com>
2022-08-27 23:26:17 +0800

    add transrecord

--- src/dai.ts
+++ src/dai.ts
@@ -1,26 +1,29 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 import { DAI, Approval, LogNote, Transfer } from "../generated/DAI/DAI";
-import { token } from "../generated/schema";
+import { token, transrecord } from "../generated/schema";
 
 export function handleTransfer(event: Transfer): void {
-//   let dai = token.load(event.address.toHexString());
-//   if (dai == null) {
-//     dai = new token(event.address.toHexString());
-//   }
-//   let daiContract = DAI.bind(event.address);
-//   dai.decimals = daiContract.decimals();
-//   dai.symbol = daiContract.symbol();
-//   dai.supply = daiContract.totalSupply();
 
-//   dai.save();
+  let id = event.block.timestamp.toHexString()
+  let dai = token.load(event.address.toHexString());
 
-    let dai = token.load(event.address.toHexString());
-    if(dai == null) {
-        dai = new token(event.address.toHexString());
-    }
-    dai.to = event.params.dst.toHexString();
-    dai.from = event.params.src.toHexString();
-    dai.amount = event.params.wad;
+  let records = transrecord.load(id);
 
-    dai.save();
+  if (dai == null) {
+    dai = new token(event.address.toHexString());
+  }
+
+  if (records == null ){
+    records = new transrecord(id);
+  }
+  let daiContract = DAI.bind(event.address);
+  dai.decimals = daiContract.decimals();
+  dai.symbol = daiContract.symbol();
+  dai.supply = daiContract.totalSupply();
+  dai.save();
+
+  records.to = event.params.dst.toHexString();
+  records.from = event.params.src.toHexString();
+  records.amount = event.params.wad;
+  records.save();
 }
