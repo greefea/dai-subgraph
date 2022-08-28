bb4685095ebf4bdc2dd294da9af78bc030b082fb
wyaming89 <wyaming89@gmail.com>
2022-08-28 17:08:03 +0800

    update handler

--- src/dai.ts
+++ src/dai.ts
@@ -1,21 +1,35 @@
-import { BigInt } from "@graphprotocol/graph-ts";
+import { BigInt, ByteArray, log} from "@graphprotocol/graph-ts";
 import { DAI, Approval, LogNote, Transfer } from "../generated/DAI/DAI";
-import { token, transrecord } from "../generated/schema";
+import { token, transfer, dailyTransfer } from "../generated/schema";
 
 export function handleTransfer(event: Transfer): void {
 
-  let id = event.block.timestamp.toHexString()
   let dai = token.load(event.address.toHexString());
 
-  let records = transrecord.load(id);
+  let records = transfer.load(event.transaction.hash.toHexString());
+  let day = (event.block.timestamp.toI32() - (event.block.timestamp.toI32()%(60*60*24))).toString();
+
+  let daily = dailyTransfer.load(day);
+
+  // let dailydata = dailyTranfer.load(event.)
+  log.info('block timestamp: {}',[event.block.timestamp.toString()])
 
   if (dai == null) {
     dai = new token(event.address.toHexString());
   }
 
   if (records == null ){
-    records = new transrecord(id);
+    records = new transfer(event.transaction.hash.toHexString());
   }
+
+  if (daily == null){
+    daily = new dailyTransfer(day);
+    daily.amount = new BigInt(0);
+    daily.transfers = 0;
+    daily.save();
+  }
+
+
   let daiContract = DAI.bind(event.address);
   dai.decimals = daiContract.decimals();
   dai.symbol = daiContract.symbol();
@@ -26,4 +40,17 @@ export function handleTransfer(event: Transfer): void {
   records.from = event.params.src.toHexString();
   records.amount = event.params.wad;
   records.save();
+
+  let transferArray = new Array<String>();
+  if (transferArray.indexOf(event.params.src.toHexString(),0)==-1){
+    transferArray.push(event.params.src.toHexString());
+    daily.transfers = daily.transfers + 1;
+  }
+  if (transferArray.indexOf(event.params.dst.toHexString(),0)==-1){
+    transferArray.push(event.params.dst.toHexString());
+    daily.transfers = daily.transfers + 1;
+  }
+
+  daily.amount = daily.amount.plus(event.params.wad);
+  daily.save();
 }
